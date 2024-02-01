55cc094265aca39198f89bb2c876f7334596feb0
zjeee <wiritipon.t@ku.th>
2024-02-01 11:35:49 +0700

    update

---
+++ my-app/src/Hello.js
@@ -0,0 +1,10 @@
+
+const Hello = (props) =>{
+    // const name = "Wiritipon Thapkhan"
+    return(
+        <>
+            <h1>Hello World,{props.name} {props.sur}</h1>
+        </>
+    );
+}
+export default Hello;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Hello from './Hello'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Hello name = "wiritipon" sur = "thapkhan"/>
   </React.StrictMode>
 );
 
