f8481ff2e3c4af696298f3909eb1ec4f444986f8
zjeeee <wiritipon.t@ku.th>
2024-02-15 09:27:53 +0700

    useState_learn

--- my-app/src/Hello.js
+++ my-app/src/Hello.js
@@ -1,9 +1,15 @@
-
-const Hello = (props) =>{
+import './Hello.css';
+import React ,{ useState } from 'react';
+const Hello = () =>{
+    const [count , setCount ] = useState(0);
     // const name = "Wiritipon Thapkhan"
     return(
         <>
-            <h1>Hello World,{props.name} {props.sur}</h1>
+            
+            <h1>Hello World,</h1>
+            <button onClick={() =>{setCount(count + 1)}}>Click</button>
+            <button onClick={() =>{setCount(0)}}>Reset</button>
+            <p>Click {count} times</p>
         </>
     );
 }
