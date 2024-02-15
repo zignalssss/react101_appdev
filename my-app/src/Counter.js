6e2f780c38d6622b0f3f5fb898b572461a1c6877
zjeeee <wiritipon.t@ku.th>
2024-02-15 11:28:56 +0700

    kk

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,11 +1,20 @@
-import {useState} from 'react';
+import {useEffect,useState} from 'react';
 import Hello from './Hello'
 export default function Counter(){
-    const [show,setShow] = useState(true);
+    // const [show,setShow] = useState(true);
+    // const bookList = ["book1","book2","book3"];
+    // const item = bookList.map(book => <li>{book}</li>)
+    useEffect(()=>{
+        
+    },[])
     return(
         <>
-        {show? <h1>hello world</h1>:<Hello/>}
+        {/* {show? <h1>hello world</h1>:<Hello/>}
         <button onClick={()=>{setShow(!show)}}> Toggle</button>
+        <ul>
+            {item}
+        </ul> */}
+
         </>
     )
 }
