c0bc39b9a415e27579e9aa1fb7ffc0c92457b931
zjeeee <wiritipon.t@ku.th>
2024-02-15 09:36:56 +0700

    onChange_useState

--- my-app/src/Hello.js
+++ my-app/src/Hello.js
@@ -2,6 +2,10 @@ import './Hello.css';
 import React ,{ useState } from 'react';
 const Hello = () =>{
     const [count , setCount ] = useState(0);
+    const [value, setValue] = useState("type here!")
+    const onTextChange= (event)=>{
+        setValue(event.target.value);
+    }
     // const name = "Wiritipon Thapkhan"
     return(
         <>
@@ -10,6 +14,8 @@ const Hello = () =>{
             <button onClick={() =>{setCount(count + 1)}}>Click</button>
             <button onClick={() =>{setCount(0)}}>Reset</button>
             <p>Click {count} times</p>
+            <input type="text" value={value} onChange={event=>onTextChange(event)}></input>
+            <h2>{value.length}</h2>
         </>
     );
 }
